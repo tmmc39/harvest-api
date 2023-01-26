1c759ecf760301357f79abeb0c7a7f669eaab5e9
CryptJS13 <jasper@slimmens.nl>
2023-01-26 18:42:21 +0100

    change PORT var

--- src/runtime/app.js
+++ src/runtime/app.js
@@ -17,7 +17,7 @@ module.exports = function () {
   initRouter(app)
 
   const httpsPort = process.env.HTTPS_PORT || 443
-  const httpPort = process.env.HTTP_PORT || 80
+  const httpPort = process.env.PORT || 80
 
   let server, port
 
